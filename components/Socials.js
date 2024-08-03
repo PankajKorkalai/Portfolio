483819d6728630da9202364b9c3b82677d506456
pankajkorkalai@gmail.com <PankajKorkalai>
2024-08-04 02:28:28 +0530

    Home page done

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,39 @@
+// links
+import Link from "next/link";
+
+// icons
+import {
+  FaGithub,
+  FaEnvelope,
+  FaLinkedin,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa"; // Importing required icons
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={"https://github.com/PankajKorkalai"} className="hover:text-accent transition-all duration-300">
+        <FaGithub /> {/* GitHub Icon */}
+      </Link>
+
+      <Link href={"mailto:your-pankajkorkalai@gmail.com"} className="hover:text-accent transition-all duration-300">
+        <FaEnvelope /> {/* Email Icon */}
+      </Link>
+
+      <Link href={"https://www.linkedin.com/in/pankaj-korkalai-711723287/"} className="hover:text-accent transition-all duration-300">
+        <FaLinkedin /> {/* LinkedIn Icon */}
+      </Link>
+
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <FaInstagram /> {/* Instagram Icon */}
+      </Link>
+
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <FaTwitter /> {/* X (Twitter) Icon */}
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
